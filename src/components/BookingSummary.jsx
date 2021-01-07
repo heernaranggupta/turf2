8b53c821e8fe244b6280ea6e1653c5f0ddfc37b4
neha433 <neha.smartypantstech@gmail.com>
2021-01-07 18:02:21 +0530

    razorpay ui done get slotes for order is left

---
+++ src/components/BookingSummary.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useCallback, useEffect } from "react";
+import classnames from "classnames";
+import styles from "../css/Cart.module.css";
+import axios from "axios";
+import api from "../config/api";
+import headerWithToken from "../config/headerWithToken";
+
+const BookingSummary = () =>{
+
+    const [bookingList,setBookingList] = useState([])
+
+    const bookingSummary = useCallback(() =>{
+        const data = JSON.parse(localStorage.getItem("turfUserDetails"))
+        axios.get(api + 'user/booking-history?userPhoneNumber=' + data.user.phoneNumber,headerWithToken).then(res=>{
+            console.log(res)
+        }).catch(err=>{
+            console.log(err.response)
+        })
+    })
+
+    useEffect(()=>{
+        bookingSummary();
+    },[bookingSummary])
+
+    return(
+        <div className={classnames("box", styles.dateCardWrapper)}>
+            <header className="card-header">
+              <p className="card-header-title has-text-white">Upcoming Booking</p>
+            </header>
+            <header className="card-header">
+              <p className="card-header-title has-text-white">Booking History</p>
+            </header>
+
+        </div>
+    );
+}
+export default BookingSummary;
