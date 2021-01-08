91e3046756b84b95d872ca4a1c90c61824255fae
neha433 <neha.smartypantstech@gmail.com>
2021-01-08 16:20:53 +0530

    place order bug solved

--- src/components/BookingSummary.jsx
+++ src/components/BookingSummary.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useCallback, useEffect } from "react";
 import classnames from "classnames";
-import styles from "../css/Cart.module.css";
+import styles from "../css/BookingSummary.module.css";
 import axios from "axios";
 import api from "../config/api";
 import headerWithToken from "../config/headerWithToken";
 import { SlotCardItem } from "./SlotCardItem";
-import cartStyles from "../css/SlotItems.module.css";
 
 const BookingSummary = () =>{
 
@@ -15,17 +14,18 @@ const BookingSummary = () =>{
     const bookingSummary = useCallback(() =>{
         const data = JSON.parse(localStorage.getItem("turfUserDetails"))
         axios.get(api + 'user/booking-history?userPhoneNumber=' + data.user.phoneNumber,headerWithToken).then(res=>{
-            debugger
+          
             console.log(res.data)
             const filterUpComing = res.data.body.bookedTimeSlots.filter(
                 function (item) {
-                    return new Date() <= new Date(item.date)
+                    console.log(new Date())
+                    return new Date() < new Date(item.date)
                 }
               );
             setUpComing(filterUpComing)
             const filterHistory = res.data.body.bookedTimeSlots.filter(
                 function (item) {
-                    return new Date() >= new Date(item.date)
+                    return new Date() > new Date(item.date)
                 }
               );
             setHistory(filterHistory)
@@ -35,18 +35,30 @@ const BookingSummary = () =>{
     },[])
 
     const handleOnClick = (index, ground, id, item) => {
-        console.log("delete booking")
+        console.log("delete",item)
+        const body ={
+            "bookingId": item.bookingId,
+            "price": item.price,
+            "turfId": item.turfId,
+            "userId": item.userId,
+            "date": item.date,
+            "startTime": item.startTime,
+            "endTime": item.endTime
+        }
+        axios.post(api + 'user/cancel-booking',body,headerWithToken).then(res=>{
+            console.log(res.data)
+        })
     }
 
     useEffect(()=>{
         bookingSummary();
-    },[bookingSummary])
+    },[])
 
     return(
         <div className={classnames("box", styles.dateCardWrapper)}>
             <header className="card-header">
               <p className="card-header-title has-text-white">Upcoming Booking</p>
-              <div className={classnames(cartStyles.slotContentWrapper)}>
+              <div className={classnames(styles.slotContentWrapper)}>
                 {upcoming &&
                     upcoming.map((item, index) => (
                     <SlotCardItem
