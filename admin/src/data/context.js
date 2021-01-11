b52ed0ea415cee075488d5e2d29917a175768652
Ferin <ferin@localhost.localdomain>
2021-01-11 15:07:17 +0530

    Additional Changes Added

--- admin/src/data/context.js
+++ admin/src/data/context.js
@@ -4,15 +4,24 @@ export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [companyName, setCompanyName] = useState("");
+  const [role, setRole] = useState("");
+  const [username, setUsername] = useState("");
 
-    return (
-        <Context.Provider
-          value={{
-            isLoggedIn,
-            setIsLoggedIn,
-        }}
-        >
-          {children}
-        </Context.Provider>
-      );
-}
+  return (
+    <Context.Provider
+      value={{
+        isLoggedIn,
+        setIsLoggedIn,
+        companyName,
+        setCompanyName,
+        role,
+        setRole,
+        username,
+        setUsername,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
--- admin/src/routes/routes.js
+++ admin/src/routes/routes.js
@@ -1,12 +1,49 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect, useContext, useCallback } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Home from "../pages/Home";
 import ProtectedRoutes from "./protected.routes";
 import Login from "../pages/Login";
 import AddManager from "../pages/AddManager";
+import { Context } from "../data/context";
 
 const Routes = () => {
+  const { setIsLoggedIn, setCompanyName, setRole, setUsername } = useContext(
+    Context
+  );
+
+  const fetchUserData = useCallback(async () => {
+    const data = await localStorage.getItem("turfAdminDetails");
+    const newData = await JSON.parse(data);
+    if (newData) {
+      try {
+        setIsLoggedIn(true);
+        setCompanyName(newData.businessResponse.companyName);
+        setRole(newData.businessResponse.role);
+        setUsername(newData.businessResponse.username);
+      } catch (error) {
+        setIsLoggedIn(false);
+        setCompanyName("");
+        setRole("");
+        setUsername("");
+      }
+    } else {
+      setIsLoggedIn(false);
+      setCompanyName("");
+      setRole("");
+      setUsername("");
+    }
+  }, [setIsLoggedIn, setCompanyName, setRole, setUsername]);
+
+  useEffect(() => {
+    fetchUserData();
+  }, [fetchUserData]);
+
   return (
     <Router>
       <Switch>
@@ -16,6 +53,8 @@ const Routes = () => {
         <ProtectedRoutes path="/addManager">
           <AddManager />
         </ProtectedRoutes>
+
+        <Redirect to="/" />
       </Switch>
       <ToastContainer pauseOnHover={false} autoClose={3000} />
     </Router>
