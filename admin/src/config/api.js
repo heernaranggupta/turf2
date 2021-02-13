cb0a459d8f3c438e5619625ac276e439576635bd
Ferin <ferin@localhost.localdomain>
2021-02-13 14:36:35 +0530

    ENV added to admin

--- admin/src/config/api.js
+++ admin/src/config/api.js
@@ -1,4 +1,4 @@
 // const api = "http://ec2-13-126-112-36.ap-south-1.compute.amazonaws.com:8080/";
 // const api = "http://192.168.29.120:8080/";
-const api = "http://turfbookingapi.ml:8080/";
+const api = process.env.REACT_APP_API_URL;
 export default api;
